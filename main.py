c1cd2b6b0e1a7ee9e0d8a3431e3934222a3f1010
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2025-11-08 15:44:20 -0800

    replaced the kahoot client code with example cuz it WONT WORK :c

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
 from kahoot import KahootClient
 from kahoot.packets.server.question_start import QuestionStartPacket
+
 async def question_start(packet: QuestionStartPacket):
-    image_url = packet.get("nextGameBlockData")
-    print(image_url)
+    print(f"Question started: {packet}")
+
 async def main():
     client = KahootClient()
     client.on("question_start", question_start)
-    game_pin = input("Enter the game PIN: ")
-    await client.join_game(game_pin=game_pin, username='your_username')
+    await client.join_game(game_pin=3850352, username='your_username')
+
+# Run the main function
 asyncio.run(main())
