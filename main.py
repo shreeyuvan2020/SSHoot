06c9b434f47cea66bd128e21813752108787d0e1
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2025-11-08 18:31:31 -0800

    Made the tui and backend work together to join da game

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from kahoot import KahootClient
 from kahoot.packets.server.question_start import QuestionStartPacket
 
 async def question_start(packet: QuestionStartPacket):
+    packet = list(vars(packet).items())
     print(f"Question started: {packet}")
 
-async def main():
+async def main(game_pin: int):
     client = KahootClient()
     client.on("question_start", question_start)
-    await client.join_game(game_pin=3850352, username='your_username')
-
-# Run the main function
-asyncio.run(main())
+    await client.join_game(game_pin=game_pin, username='your_username')
--- tui.py
+++ tui.py
@@ -4,6 +4,8 @@ from textual.app import App, ComposeResult, Screen
 from textual.widgets import Static, Input, Label, Button, Link, Header
 from textual.containers import Container, Horizontal, VerticalScroll, Vertical
 from art import text2art
+from main import main
+import asyncio
 class InputApp(App):
     CSS_PATH = "kahoot.tcss"
     def compose(self) -> ComposeResult:
@@ -11,6 +13,10 @@ class InputApp(App):
         ascii_art = text2art("SSHoot!",)
         yield Label(ascii_art, id="land_label")
         yield Input(placeholder="Game PIN", id="game_pin_input")
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        game_pin = int(event.value)
+        print("yooooo")
+        asyncio.create_task(main(game_pin))
     def on_mount(self) -> None:
         self.title = "SSHoot!"
 
